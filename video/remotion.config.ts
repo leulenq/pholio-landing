85e66edcedf8fc2d850f81b8840242bb8fe560cd
leulenq <>
2026-06-14 04:47:49 -0400

    chore: remove video directory

--- video/remotion.config.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from "@remotion/cli/config";
-
-// Velvet-ink launch film. Crisp stills, full quality, deterministic output.
-Config.setVideoImageFormat("jpeg");
-Config.setOverwriteOutput(true);
-Config.setConcurrency(null); // let Remotion pick based on cores
-Config.setChromiumOpenGlRenderer("angle");
